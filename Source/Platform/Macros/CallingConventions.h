41b4be3527f1440b076263e5043cc1d18da9794a
Mthia727 <mathiasgj1.1234@gmail.com>
2023-06-29 00:12:40 +0200

    Revert "2d89f1e0ed937240"
    This reverts commit 30777c2066417398cf02cdd168c32d405680f95d.

--- Source/Platform/Macros/CallingConventions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "PlatformSpecific.h"
-
-#define THISCALL_CONV WIN32_ARGS(__thiscall)
-#define FASTCALL_CONV WIN32_ARGS(__fastcall)
-#define CDECL_CONV    WIN32_ARGS(__cdecl)
-#define STDCALL_CONV  WIN32_ARGS(__stdcall)
