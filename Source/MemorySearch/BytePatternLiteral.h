1a851131ac33096096b1609acfb5642f22c42c8b
Mthia727 <mathiasgj1.1234@gmail.com>
2023-06-28 23:39:49 +0200

    Revert "7d4d14661097bc7a"
    This reverts commit 3b5c4c20166bc497930d875d8506325dc46a9120.

--- Source/MemorySearch/BytePatternLiteral.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "BytePatternStorage.h"
-
-template <BytePatternStorage storage>
-consteval auto operator"" _pat()
-{
-    return BytePatternStorage<storage.size>{ storage };
-}
