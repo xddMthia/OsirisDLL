b64199ca02adbb111c7c8661f821c6d33a1b4cf5
Mthia727 <mathiasgj1.1234@gmail.com>
2023-06-28 23:12:59 +0200

    7122fab9f990b618

---
+++ Source/Platform/Linux/LinuxDynamicLibrary.h
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <cstddef>
+#include <cstring>
+#include <span>
+
+#include <dlfcn.h>
+#include <elf.h>
+#include <fcntl.h>
+#include <link.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#include <Utils/SafeAddress.h>
+
+template <typename PlatformApi>
+class LinuxDynamicLibrary {
+public:
+    LinuxDynamicLibrary(const char* libraryName)
+        : handle{ getModuleHandle(libraryName) } {}
+
+    [[nodiscard]] explicit operator bool() const noexcept
+    {
+        return handle != nullptr;
+    }
+
+    [[nodiscard]] SafeAddress getFunctionAddress(const char* functionName) const noexcept
+    {
+        if (handle)
+            return SafeAddress{ std::uintptr_t(PlatformApi::dlsym(handle, functionName)) };
+        return SafeAddress{ 0 };
+    }
+
+    [[nodiscard]] link_map* getLinkMap() const noexcept
+    {
+        link_map* map = nullptr;
+        if (handle)
+            PlatformApi::dlinfo(handle, RTLD_DI_LINKMAP, &map);
+        return map;
+    }
+
+    [[nodiscard]] std::span<const std::byte> getCodeSection() const noexcept
+    {
+        void* base = nullptr;
+        std::size_t size = 0;
+
+        const auto linkMap = getLinkMap();
+        if (linkMap) {
+            if (const auto fd = PlatformApi::open(linkMap->l_name, O_RDONLY); fd >= 0) {
+                if (struct stat st; PlatformApi::fstat(fd, &st) == 0) {
+                    if (const auto map = PlatformApi::mmap(nullptr, st.st_size, PROT_READ, MAP_PRIVATE, fd, 0); map != MAP_FAILED) {
+                        const auto ehdr = (ElfW(Ehdr)*)map;
+                        const auto shdrs = (ElfW(Shdr)*)(std::uintptr_t(ehdr) + ehdr->e_shoff);
+                        const auto strTab = (const char*)(std::uintptr_t(ehdr) + shdrs[ehdr->e_shstrndx].sh_offset);
+
+                        for (auto i = 0; i < ehdr->e_shnum; ++i) {
+                            const auto shdr = (ElfW(Shdr)*)(std::uintptr_t(shdrs) + i * ehdr->e_shentsize);
+
+                            if (std::strcmp(strTab + shdr->sh_name, ".text") != 0)
+                                continue;
+
+                            base = (void*)(linkMap->l_addr + shdr->sh_offset);
+                            size = shdr->sh_size;
+                            PlatformApi::munmap(map, st.st_size);
+                            PlatformApi::close(fd);
+                            break;
+                        }
+                        PlatformApi::munmap(map, st.st_size);
+                    }
+                }
+                PlatformApi::close(fd);
+            }
+        }
+        return { reinterpret_cast<const std::byte*>(base), size };
+    }
+
+private:
+    [[nodiscard]] void* getModuleHandle(const char* libraryName)
+    {
+        const auto handle = PlatformApi::dlopen(libraryName, RTLD_LAZY | RTLD_NOLOAD);
+        if (handle)
+            PlatformApi::dlclose(handle);
+        return handle;
+    }
+
+    void* handle = nullptr;
+};
