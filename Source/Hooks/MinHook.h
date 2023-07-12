3fe7c4a990773b7cb8e47961eba91b107b136a34
Mthia727 <mathiasgj1.1234@gmail.com>
2023-07-12 12:27:12 +0200

    5927624904a446b2

--- Source/Hooks/MinHook.h
+++ Source/Hooks/MinHook.h
@@ -5,14 +5,18 @@
 
 #include <Platform/Macros/CallingConventions.h>
 #include <Platform/Macros/IsPlatform.h>
+#include <Vmt/VmtLengthCalculator.h>
 
 class MinHook {
 public:
+    explicit MinHook(VmtLengthCalculator)
+    {
+    }
+
     void init(void* base) noexcept;
     void restore() noexcept {}
     std::uintptr_t hookAt(std::size_t index, void* fun) noexcept;
 
 private:
     void* base;
-    std::unique_ptr<uintptr_t[]> originals;
 };
