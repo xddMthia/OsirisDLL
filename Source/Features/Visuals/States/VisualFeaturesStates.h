fbbf564f6caf382bc982a406a28d60b773e2003c
xddMthia <mthia@xdd.moe>
2024-01-11 23:27:16 +0100

    acb0d322f2cc50b0

---
+++ Source/Features/Visuals/States/VisualFeaturesStates.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "ScopeOverlayRemoverState.h"
+#include "SniperScopeBlurRemovalFeatureState.h"
+
+struct VisualFeaturesStates {
+    ScopeOverlayRemoverState scopeOverlayRemoverState;
+    SniperScopeBlurRemovalFeatureState sniperScopeBlurRemovalFeatureState;
+};
